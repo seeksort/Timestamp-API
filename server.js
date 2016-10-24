b15f6e4c717833b4c4351036e48707bf017f88ad
Kristin Faner <kfaner@gmail.com>
2016-10-24 08:50:21 -0500

    cleaned up code

--- server.js
+++ server.js
@@ -3,18 +3,13 @@ var url = require('url');
 var fs = require('fs');
 
 var PORT = 3000;
-
 var server = http.createServer(handleRequest);
-
 var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-
+var unixTime, naturalTime;
 
 function handleRequest(req, res) {
-    var newObj = {
-        "unix": "",
-        "natural": ""
-    }
     var urlParts = url.parse(req.url);
+    // Determine if first part of search term is in months array, this determines how to handle the timestamp processing
     var searchTerm = new RegExp(urlParts.pathname.slice(1,3), 'i');
     var match = '';
     months.forEach(function(item, index){
@@ -23,51 +18,39 @@ function handleRequest(req, res) {
             match = months[index];
         }
     });
-
+    //Direct to main page
     if ((urlParts.pathname === '/') || (urlParts.pathname === '')){
         fs.readFile('index.html', function(err, data) {
             if (err) {
                 console.log(err);
             }
             else {
-                console.log('success')
                 res.writeHead(200, {'Content-Type': 'text/html'});
                 res.end(data);
             }
         });
     }
-    else if (match !== '') {
-        console.log('path 1')
-        var dateStrArray = urlParts.pathname.slice(1).split('%20');
-        var month = match;
-        var day = dateStrArray[1];
-        var year = dateStrArray[2];
-        if (day.slice(-1) === ',') {
-            day = day.slice(0,-1);
-        }
-        var tempTime = new Date(parseInt(year), months.indexOf(month), parseInt(day));
-        var naturalTime = month + ' ' + day + ', ' + year;
-        console.log(tempTime)
-        console.log(tempTime.getTime()/1000)
-        newObj["unix"] = tempTime.getTime()/1000;
-        newObj["natural"] = naturalTime;
-        console.log(newObj)
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end('<div>'+JSON.stringify(newObj)+'</div>');
-        console.log(JSON.stringify(newObj));
-    }
+    // Processes pathname differently if user entered natural language or unix
     else {
-        console.log('path 2')
-        var timeStr = parseInt(urlParts.pathname.slice(1)) * 1000;
-        var tempTime = new Date(timeStr);
-        var naturalTime = "";
-        naturalTime += months[tempTime.getMonth()] + ' ' + tempTime.getDate() + ', ' + tempTime.getFullYear();
-        newObj["unix"] = tempTime.getTime()/1000;
-        newObj["natural"] = naturalTime;
-        console.log(newObj)
+        if (match !== '') {
+            var dateStrArray = urlParts.pathname.slice(1).split('%20');
+            var month = match;
+            var day = dateStrArray[1];
+            var year = dateStrArray[2];
+            if (day.slice(-1) === ',') {
+                day = day.slice(0,-1);
+            }
+            unixTime = new Date(parseInt(year), months.indexOf(month), parseInt(day));
+            naturalTime = month + ' ' + day + ', ' + year;
+        }
+        else {
+            var timeStr = parseInt(urlParts.pathname.slice(1)) * 1000;
+            unixTime = new Date(timeStr);
+            naturalTime = months[unixTime.getMonth()] + ' ' + unixTime.getDate() + ', ' + unixTime.getFullYear();
+        }
+        var timestampObj = {"unix": unixTime.getTime()/1000, "natural": naturalTime};
         res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end('<div>'+JSON.stringify(newObj)+'</div>');
-        console.log(JSON.stringify(newObj));
+        res.end(JSON.stringify(timestampObj));
     }
 }
 
